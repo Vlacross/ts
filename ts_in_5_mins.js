7a36218cfc78233b3931efeaf0f0767ae4059c68
Tim Vlasuk <timvlasuk@gmail.com>
2020-04-06 23:09:58 -0700

    added class and html file

--- ts_in_5_mins.js
+++ ts_in_5_mins.js
@@ -1,5 +1,15 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = ['0', '1', '2', '3'];
+var user = new Student("Jane", "P", "Dooser");
 document.body.textContent = greeter(user);
+/* coding along with quick guide from: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html */ 
--- ts_in_5_mins.ts
+++ ts_in_5_mins.ts
@@ -1,3 +1,12 @@
+
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -7,6 +16,9 @@ function greeter(person: Person) {
   return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-let user = { firstName: "Jane", lastName: "Dooser" };
+let user = new Student("Jane", "P", "Dooser");
 
 document.body.textContent = greeter(user);
+
+
+/* coding along with quick guide from: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html */
